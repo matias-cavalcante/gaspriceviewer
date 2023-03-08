d8701badbbed0fa9c40fb9ea369bc438b40c14d8
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-03-08 01:43:16 +0000

    Get hour correctly

--- Script.js
+++ Script.js
@@ -1,22 +1,20 @@
+const box = document.getElementById("results");
 
-const box = document.getElementById("results")
+// Define the API endpoint URL
+const apiUrl = 'https://gas-prices-iceland.onrender.com/orkan';
 
-
-const now = new Date();
-const hour = now.getHours();
-
-
-// 1. Define the API endpoint URL
-const apiUrl = 'http://127.0.0.1:5000/orkan';
-
-
+// Set the interval to fetch data from the API every 5 minutes
 setInterval(function() {
-
-// 2. Fetch data from the API
-fetch(apiUrl)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data['borgarnes'])
-    box.append(data['borgarnes'], " ", hour)
-  })}, 500000);
-
+  // Get the current date and time
+  const now = new Date();
+  // Get the current hour
+  const hour = now.getHours();
+  // Fetch data from the API
+  fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data['borgarnes'])
+      // Append the data and current hour to the HTML element
+      box.append(data['borgarnes'], " ", hour)
+    })
+}, 50000); //
