a3963a71ea9c55f873a191d2c975e7973c41beb8
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-03-17 01:33:08 +0000

    Mobile styling and components in place

--- Script.js
+++ Script.js
@@ -1,6 +1,28 @@
+const reveal = document.getElementById("choose")
+const buttns = document.getElementById("regions")
+
+
 const divTableContainer = document.getElementById("table-container");
 const table = document.createElement("table");
 table.classList.add('table-style')
+
+const bensinButton = document.getElementById("ben")
+const diselButton = document.getElementById("dis")
+
+bensinButton.addEventListener("click", function(){
+  fuel = 'bensin'
+  bensinButton.style.backgroundColor = "#011826"
+  diselButton.style.backgroundColor = "#02436c"
+
+})
+
+diselButton.addEventListener("click", function(){
+  fuel = 'disel'
+  diselButton.style.backgroundColor = "#011826"
+  bensinButton.style.backgroundColor = "#02436c"
+})
+
+
 
 let urlTemplate = 'https://gas-prices-iceland.onrender.com/'
 const n1Url = urlTemplate + "n1";
@@ -15,6 +37,22 @@ const stationsBuild = [
   { code: 'orkan', url: orkanUrl, img: 'logos/orkan.png', time: 600200 },
   { code: 'n1', url: n1Url, img: 'logos/n1.png', time: 600300 },
   { code: 'atl',url: atloliaUrl, img: 'logos/atlantsolia.png', time: 600500 }  ];
+
+let revealState = false;
+
+reveal.addEventListener("click", function(){
+  if (revealState == false){
+    buttns.style.display = "flex"
+    buttns.style.flexDirection = "column"
+    buttns.style.height = "65vh"
+    buttns.style.justifyContent = "space-around"
+    revealState = true
+  }else if(revealState == true){
+    buttns.style.display = "none"
+    revealState = false;
+  }
+})
+
 
 function updatePrices(url) {
   return fetch(url)
@@ -40,6 +78,7 @@ function fillRow(imageurl, name, price, row){
 
   const pricecell = document.createElement("th");
   pricecell.style.fontSize = "1.3em"
+  pricecell.style.color = "#0c5380"  
   pricecell.appendChild(document.createTextNode(price))
 
   row.appendChild(logocell);
@@ -47,11 +86,11 @@ function fillRow(imageurl, name, price, row){
   row.appendChild(pricecell);
 }
 
-function newRows(filtered, imglink, gasprice) {
+function newRows(filtered, imglink, gasprice, type) {
   return filtered.map(station => {
     let stationUpperFirst = station[0].toUpperCase() + station.slice(1);
     const pricesRow = document.createElement('tr');
-    fillRow(imglink, stationUpperFirst, gasprice[station].bensin, pricesRow);
+    fillRow(imglink, stationUpperFirst, gasprice[station][type], pricesRow);
     pricesRow.classList.add('table-row-style');
     return pricesRow;
   }).sort((a, b) => parseFloat(a.lastElementChild.textContent) - parseFloat(b.lastElementChild.textContent));
@@ -62,14 +101,14 @@ function clearTable(tableElement) {
   rows.forEach(row => row.remove());
 }
 
-function displayPrices(tableElement, imgurl, link, newrows, area) {
+function displayPrices(tableElement, imgurl, link, newrows, area, gas) {
   updatePrices(link)
     .then(data => {
       const filteredStations = Object.keys(data)
         .filter(station => data[station].region === area);
       // Get rows in the table + make more rows from API all & sort them
       const rows = Array.from(tableElement.querySelectorAll('tr'));
-      const moreRows = newrows(filteredStations, imgurl, data);
+      const moreRows = newrows(filteredStations, imgurl, data, gas);
       rows.sort((a, b) => parseFloat(a.lastElementChild.textContent) - parseFloat(b.lastElementChild.textContent));
       let mergedRows = [...rows, ...moreRows].sort((a, b) => parseFloat(a.lastElementChild.textContent) - parseFloat(b.lastElementChild.textContent));
 
@@ -79,58 +118,94 @@ function displayPrices(tableElement, imgurl, link, newrows, area) {
 }
 
 
-function stationsPerRegion(region, stations){
+function stationsPerRegion(region, stations, oil){
   for (let st = 0; st < stations.length; st++){
     updatePrices(stations[st]['url']);
     setInterval(() => updatePrices(stations[st]['url']), stations[st]['time']);
-    setInterval(displayPrices(table, stations[st]['img'], stations[st]['url'], newRows, region),
+    setInterval(displayPrices(table, stations[st]['img'], stations[st]['url'], newRows, region, oil),
     stations[st]['time'])
   }
 }
 
+
+let fuel = 'disel'
+
 //Buttons to display stations & prices in each region of the country
 const capital = document.getElementById("capital")
-capital.addEventListener("click", function(){
+capital.addEventListener("click", function(){ 
   clearTable(table)
-  stationsPerRegion("Höfuðborgarsvæðið", stationsBuild)
+  if (window.innerWidth < 650){
+    buttns.style.display = "none";
+  }
+  stationsPerRegion("Höfuðborgarsvæðið", stationsBuild, fuel)
 })
 
 const south = document.getElementById("south")
 south.addEventListener("click", function(){
   clearTable(table)
-  stationsPerRegion("Suðurland", stationsBuild)
+  if (window.innerWidth < 650){
+    buttns.style.display = "none";
+  }
+  stationsPerRegion("Suðurland", stationsBuild, fuel)
 })
 
 const north = document.getElementById("north")
 north.addEventListener("click", function(){
   clearTable(table)
-  stationsPerRegion("Norðurland", stationsBuild)
+  if (window.innerWidth < 650){
+    buttns.style.display = "none";
+  }
+  stationsPerRegion("Norðurland", stationsBuild, fuel)
 })
 
 const westfjords = document.getElementById("westfjords")
 westfjords.addEventListener("click", function(){
   clearTable(table)
-  stationsPerRegion("Vestfirðir", stationsBuild)
+  if (window.innerWidth < 650){
+    buttns.style.display = "none";
+  }
+  stationsPerRegion("Vestfirðir", stationsBuild, fuel)
 })
 
 const west = document.getElementById("west")
 west.addEventListener("click", function(){
   clearTable(table)
-  stationsPerRegion("Vesturland", stationsBuild)
+  if (window.innerWidth < 650){
+    buttns.style.display = "none";
+  }
+  stationsPerRegion("Vesturland", stationsBuild, fuel)
 })
 
 const east = document.getElementById("east")
 east.addEventListener("click", function(){
   clearTable(table)
-  stationsPerRegion("Austurland", stationsBuild)
+  if (window.innerWidth < 650){
+    buttns.style.display = "none";
+  }
+  stationsPerRegion("Austurland", stationsBuild, fuel)
 })
 
 const southwest = document.getElementById("southwest")
 southwest.addEventListener("click", function(){
   clearTable(table)
-  stationsPerRegion("Suðvesturhornið", stationsBuild)
+  if (window.innerWidth < 650){
+    buttns.style.display = "none";
+  }
+  stationsPerRegion("Suðvesturhornið", stationsBuild, fuel)
 })
 
 //Initial call for content display in page
-stationsPerRegion("Höfuðborgarsvæðið", stationsBuild);
+stationsPerRegion("Höfuðborgarsvæðið", stationsBuild, fuel);
+
+window.addEventListener('resize', function() {
+  if (window.innerWidth > 650) {
+    buttns.style.display = "flex"
+    buttns.style.height = "325px"
+
+    }else if (window.innerWidth < 650){
+        buttns.style.display = "none"
+    }
+});
+
+
 
