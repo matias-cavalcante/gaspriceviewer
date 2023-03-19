9e5690b8623939b9b2328127c2cba7ea9ed710a7
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-03-19 11:02:46 +0000

    Changed colors in called to action

--- Script.js
+++ Script.js
@@ -102,7 +102,7 @@ function fillRow(imageurl, name, price, row){
 
   const pricecell = document.createElement("th");
   pricecell.style.fontSize = "1.3em"
-    pricecell.style.color = "#1164b7"
+    pricecell.style.color = "#1F8A70"
   pricecell.appendChild(document.createTextNode(price))
 
   row.appendChild(logocell);
