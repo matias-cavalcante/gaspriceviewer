491b550ff7e77db3055474f47288f3f848d45a7e
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-04-17 23:15:06 +0000

    Testing markers on map

--- maps.js
+++ maps.js
@@ -1,12 +1,27 @@
+let mapInstance;
 
 function initMap() {
-    new google.maps.Map(document.getElementById("map"), {
-      mapId: "d6c7877e45ecfbc3",
-      center: { lat: 64.9631, lng: -19.0208 },
-      zoom: 12, // Adjust the zoom level as needed
-    });
-  }
-  
-  window.initMap = initMap;
-  
-  
+  mapInstance = new google.maps.Map(document.getElementById("map"), {
+    mapId: "d6c7877e45ecfbc3",
+    center: { lat: 64.9631, lng: -19.0208 },
+    zoom: 7,
+  });
+
+  // Add the marker after the map has been initialized
+  addMarker(64.14657013, -21.89452573);
+
+  return mapInstance;
+}
+
+window.initMap = initMap;
+
+function addMarker(lat, lng) {
+  const marker = new google.maps.Marker({
+    position: { lat, lng },
+    map: mapInstance,
+  });
+
+  return marker;
+}
+
+window.addMarker = addMarker;
