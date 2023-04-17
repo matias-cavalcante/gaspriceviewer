398c89e3cbf94d267a5a8fe6cc6d1d62adc3df17
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-04-17 22:20:35 +0000

    Adjusting coordenates to show Iceland

--- Script.js
+++ Script.js
@@ -193,8 +193,8 @@ let fuel = "bensin";
 function initMap() {
   new google.maps.Map(document.getElementById("map"), {
     mapId: "d6c7877e45ecfbc3",
-    center: { lat: 48.85, lng: 2.35 },
-    zoom: 12,
+    center: { lat: 64.9631, lng: -19.0208 },
+    zoom: 7, // Adjust the zoom level as needed
   });
 }
 
