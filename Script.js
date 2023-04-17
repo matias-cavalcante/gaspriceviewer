6245f499d378c96d20ef310769ebc3e7c1717033
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-04-17 22:51:12 +0000

    Separated the maps code into a new independent file

--- Script.js
+++ Script.js
@@ -189,16 +189,3 @@ stationsPerRegion(constants.currentRegion.textContent, constants.stationsBuild, 
 let fuel = "bensin";
 
 
-
-function initMap() {
-  new google.maps.Map(document.getElementById("map"), {
-    mapId: "d6c7877e45ecfbc3",
-    center: { lat: 64.9631, lng: -19.0208 },
-    zoom: 7, // Adjust the zoom level as needed
-  });
-}
-
-window.initMap = initMap;
-
-
-
---
+++ maps.js
@@ -0,0 +1,12 @@
+
+function initMap() {
+    new google.maps.Map(document.getElementById("map"), {
+      mapId: "d6c7877e45ecfbc3",
+      center: { lat: 64.9631, lng: -19.0208 },
+      zoom: 7, // Adjust the zoom level as needed
+    });
+  }
+  
+  window.initMap = initMap;
+  
+  
