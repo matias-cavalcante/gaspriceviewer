7cc8e4adfb9c67bae2479d3941f3bfe73f01f884
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-04-17 23:02:25 +0000

    Reduced zoom on map

--- maps.js
+++ maps.js
@@ -3,7 +3,7 @@ function initMap() {
     new google.maps.Map(document.getElementById("map"), {
       mapId: "d6c7877e45ecfbc3",
       center: { lat: 64.9631, lng: -19.0208 },
-      zoom: 7, // Adjust the zoom level as needed
+      zoom: 12, // Adjust the zoom level as needed
     });
   }
   
