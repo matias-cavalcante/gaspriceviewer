34a3ecea87cb39434f3907ae360e94e204d19ff5
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-04-19 23:54:09 +0000

    Trying to ask user their position and show them on the map

--- maps.js
+++ maps.js
@@ -7,21 +7,58 @@ async function fetchData() {
 }
 
 function initMap(data) {
-  mapInstance = new google.maps.Map(document.getElementById("map"), {
-    mapId: "d6c7877e45ecfbc3",
-    center: { lat: 64.9631, lng: -19.0208 },
-    zoom: 5,
-  });
+    mapInstance = new google.maps.Map(document.getElementById("map"), {
+      mapId: "d6c7877e45ecfbc3",
+      center: { lat: 64.9631, lng: -19.0208 },
+      zoom: 5,
+    });
+  
+    // Loop through the gas station data and add markers for each location
+    data.results.forEach(gasStation => {
+      addMarker(gasStation.geo.lat, gasStation.geo.lon, gasStation.name, `Name: ${gasStation.name}<br>Bensin 95: ${gasStation.bensin95}<br>Diesel: ${gasStation.diesel}`);
+    });
+  
+    // Request user location
+    getUserLocation();
+  
+    return mapInstance;
+  }
 
-  // Loop through the gas station data and add markers for each location
-  data.results.forEach(gasStation => {
-    addMarker(gasStation.geo.lat, gasStation.geo.lon, gasStation.name, `Name: ${gasStation.name}<br>Bensin 95: ${gasStation.bensin95}<br>Diesel: ${gasStation.diesel}`);
-  });
 
-  return mapInstance;
-}
+
+  function getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        position => {
+          const userPosition = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+  
+          // Add a marker for the user's location
+          const userMarker = addMarker(userPosition.lat, userPosition.lng, "Your Location", "You are here");
+          userMarker.setIcon("http://maps.google.com/mapfiles/ms/icons/green-dot.png");
+  
+          // Center the map on the user's location
+          mapInstance.setCenter(userPosition);
+          mapInstance.setZoom(12);
+        },
+        error => {
+          console.error("Error obtaining user location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }
 
 window.initMap = initMap;
+
+
+
+
+
+
 
 function addMarker(lat, lng, title, content) {
   const marker = new google.maps.Marker({
