71c67f28a977eacca97b40e8ebab3e65976deaf4
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-04-20 00:36:09 +0000

    Going back to previous state of javascript with error

--- maps.js
+++ maps.js
@@ -2,57 +2,49 @@ function init() {
     let mapInstance;
   
     async function fetchData() {
-        const response = await fetch('https://apis.is/petrol');
-        const data = await response.json();
-        return data;
-      }      
+      const response = await fetch('https://apis.is/petrol');
+      const data = await response.json();
+      initMap(data);
+    }
   
-    async function initMap() {
-        const data = await fetchData();
-      
-        mapInstance = new google.maps.Map(document.getElementById("map"), {
-          mapId: "d6c7877e45ecfbc3",
-          center: { lat: 64.9631, lng: -19.0208 },
-          zoom: 5,
-        });
-      
-        // Loop through the gas station data and add markers for each location
-        data.results.forEach(gasStation => {
-          addMarker(gasStation.geo.lat, gasStation.geo.lon, gasStation.name, `Name: ${gasStation.name}<br>Bensin 95: ${gasStation.bensin95}<br>Diesel: ${gasStation.diesel}`);
-        });
-      
-        // Request user location
-        getUserLocation();
-      
-        return mapInstance;
-      }
-      
+    function initMap(data) {
+      mapInstance = new google.maps.Map(document.getElementById("map"), {
+        mapId: "d6c7877e45ecfbc3",
+        center: { lat: 64.9631, lng: -19.0208 },
+        zoom: 5,
+      });
+  
+      data.results.forEach(gasStation => {
+        addMarker(gasStation.geo.lat, gasStation.geo.lon, gasStation.name, `Name: ${gasStation.name}<br>Bensin 95: ${gasStation.bensin95}<br>Diesel: ${gasStation.diesel}`);
+      });
+  
+      getUserLocation();
+    }
+  
     function getUserLocation() {
-        if (navigator.geolocation) {
-          setTimeout(() => {
-            navigator.geolocation.getCurrentPosition(
-              position => {
-                const userPosition = {
-                  lat: position.coords.latitude,
-                  lng: position.coords.longitude,
-                };
-      
-                const userMarker = addMarker(userPosition.lat, userPosition.lng, "Your Location", "You are here");
-                userMarker.setIcon("http://maps.google.com/mapfiles/ms/icons/green-dot.png");
-      
-                mapInstance.setCenter(userPosition);
-                mapInstance.setZoom(12);
-              },
-              error => {
-                console.error("Error obtaining user location:", error);
-              }
-            );
-          }, 1000);
-        } else {
-          console.error("Geolocation is not supported by this browser.");
-        }
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            const userPosition = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude,
+            };
+  
+            const userMarker = addMarker(userPosition.lat, userPosition.lng, "Your Location", "You are here");
+            userMarker.setIcon("http://maps.google.com/mapfiles/ms/icons/green-dot.png");
+  
+            mapInstance.setCenter(userPosition);
+            mapInstance.setZoom(12);
+          },
+          error => {
+            console.error("Error obtaining user location:", error);
+          }
+        );
+      } else {
+        console.error("Geolocation is not supported by this browser.");
       }
-      
+    }
+  
     function addMarker(lat, lng, title, content) {
       const marker = new google.maps.Marker({
         position: { lat, lng },
