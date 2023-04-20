b378d9b733d256cb6296523c7a7551755a7d0690
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-04-20 00:28:56 +0000

    Trying new approach by changing html and js

--- maps.js
+++ maps.js
@@ -2,25 +2,31 @@ function init() {
     let mapInstance;
   
     async function fetchData() {
-      const response = await fetch('https://apis.is/petrol');
-      const data = await response.json();
-      initMap(data);
-    }
+        const response = await fetch('https://apis.is/petrol');
+        const data = await response.json();
+        return data;
+      }      
   
-    function initMap(data) {
-      mapInstance = new google.maps.Map(document.getElementById("map"), {
-        mapId: "d6c7877e45ecfbc3",
-        center: { lat: 64.9631, lng: -19.0208 },
-        zoom: 5,
-      });
-  
-      data.results.forEach(gasStation => {
-        addMarker(gasStation.geo.lat, gasStation.geo.lon, gasStation.name, `Name: ${gasStation.name}<br>Bensin 95: ${gasStation.bensin95}<br>Diesel: ${gasStation.diesel}`);
-      });
-  
-      getUserLocation();
-    }
-  
+    async function initMap() {
+        const data = await fetchData();
+      
+        mapInstance = new google.maps.Map(document.getElementById("map"), {
+          mapId: "d6c7877e45ecfbc3",
+          center: { lat: 64.9631, lng: -19.0208 },
+          zoom: 5,
+        });
+      
+        // Loop through the gas station data and add markers for each location
+        data.results.forEach(gasStation => {
+          addMarker(gasStation.geo.lat, gasStation.geo.lon, gasStation.name, `Name: ${gasStation.name}<br>Bensin 95: ${gasStation.bensin95}<br>Diesel: ${gasStation.diesel}`);
+        });
+      
+        // Request user location
+        getUserLocation();
+      
+        return mapInstance;
+      }
+      
     function getUserLocation() {
         if (navigator.geolocation) {
           setTimeout(() => {
@@ -47,7 +53,6 @@ function init() {
         }
       }
       
-  
     function addMarker(lat, lng, title, content) {
       const marker = new google.maps.Marker({
         position: { lat, lng },
