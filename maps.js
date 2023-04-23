ef5c99701b434320c810ee01a284cc19341240d1
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-04-23 16:55:36 +0000

    New attempt to correct flow of things when pressing choosecompany

--- maps.js
+++ maps.js
@@ -19,7 +19,7 @@ function init() {
         const data = await response.json();
         initMap(data);
 
-        
+        /*
         chooseCompany.addEventListener("click", function(){
             companiesCarousel.classList.toggle("hidden");
             companiesCarousel.classList.toggle("carousel-cont");
@@ -34,7 +34,44 @@ function init() {
                 clearMarkers()
                 addMarkersByCompany(companies[0], data)
             }
-        })
+        })*/
+
+
+        chooseCompany.addEventListener("click", function() {
+            companiesCarousel.classList.toggle("hidden");
+            companiesCarousel.classList.toggle("carousel-cont");
+        
+            if (chooseCompany.textContent === "Choose company") {
+                chooseCompany.textContent = "See all companies";
+                clearMarkers();
+                // Show all markers
+                data.results.forEach(gasStation => {
+                    addMarker(
+                        gasStation.geo.lat,
+                        gasStation.geo.lon,
+                        gasStation.name,
+                        `${gasStation.company} ${gasStation.name}<br>Bensin 95: ${gasStation.bensin95}<br>Diesel: ${gasStation.diesel}`,
+                        "logos/fuel-station.png"
+                    );
+                });
+            } else if (chooseCompany.textContent === "See all companies") {
+                chooseCompany.textContent = "Choose company";
+                clearMarkers();
+                // Show markers only for the first company in the array
+                data.results.forEach(gasStation => {
+                    if (gasStation.company === companies[0]) {
+                        addMarker(
+                            gasStation.geo.lat,
+                            gasStation.geo.lon,
+                            gasStation.name,
+                            `${gasStation.company} ${gasStation.name}<br>Bensin 95: ${gasStation.bensin95}<br>Diesel: ${gasStation.diesel}`,
+                            "logos/fuel-station.png"
+                        );
+                    }
+                });
+            }
+        });
+        
 
         arrowLeft.addEventListener("click", function(){
             let selectedCompany = "";
