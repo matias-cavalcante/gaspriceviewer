3a71ee97d08ffb24740ec1e0860628bedec575d1
matias-cavalcante <matias.cavalcante93@gmail.com>
2023-04-30 22:06:18 +0000

    Icons are being passed invalid path in some cases. Checking

--- maps.js
+++ maps.js
@@ -87,32 +87,18 @@ function init() {
         });
       }
     
-    /*
-
-    function getCompanyName(name){
-        const parts = name.split(" "); // Split the string on the space
-        const firstPart = parts[0]; // Get the first element of the array
-        let img =  "markers/" + firstPart.toLowerCase() + ".png"
-        img.style.height = "70px";
-        return img
-    }*/
-
-
     function getCompanyName(name) {
         const parts = name.split(" "); // Split the string on the space
         const firstPart = parts[0]; // Get the first element of the array
         const imgSrc = "markers/" + firstPart.toLowerCase() + ".png";
+        console.log("IMG PATH  ",   imgSrc )
         const icon = {
           url: imgSrc,
           scaledSize: new google.maps.Size(40, 36), // Set the desired dimensions
         };
         return icon;
       }
-      
-
-
-
-      
+        
     let mapInstance;
     let currentInfoWindow;
 
